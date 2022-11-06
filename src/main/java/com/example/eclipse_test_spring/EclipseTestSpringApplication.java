ba2a073403ed8e70b52e7e962d59cff385e80a8f
Nilanjan Roy <nilanjanroy@Nilanjans-MacBook-Pro.local>
2022-11-06 12:20:12 +0530

    Initial commit

---
+++ src/main/java/com/example/eclipse_test_spring/EclipseTestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.eclipse_test_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclipseTestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclipseTestSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eclipse_test_spring/EclipseTestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eclipse_test_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclipseTestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
